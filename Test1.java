535d01bfa1ba431caeb128655e39b52fb86deada
GUGULOTH SIREESHA <108211623+sireeshaguguloth@users.noreply.github.com>
2024-05-12 19:12:38 +0530

    Add files via upload
    Exception Example

--- Test1.java
+++ Test1.java
@@ -1,39 +1,25 @@
-package com.siri.inheritance.introduction;
-class ClassOne{
-	protected void m1() {
-		System.out.println("business logic1");
-	}
-}
-	
-class ClassTwo extends ClassOne{
-	protected void m2() {
-		System.out.println("business logic2");
-	}
-}
-class ClassThree extends ClassTwo{
-	protected void m3() {
-		System.out.println("business logic3");
-	}
-}
-class ClassFour extends ClassTwo{
-	protected void m4() {
-		System.out.println("business logic4");
-	}
-}
-
+package com.siri.exception;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-	ClassOne ob1=new ClassOne();
-	ob1.m1();
-	ClassTwo ob2=new ClassTwo();
-	ob2.m2();
-	ClassThree ob3=new ClassThree();
-	ob3.m3();
-	ClassFour ob4=new ClassFour();
-	ob4.m4();
-	
+		try {
+		int a,b,c;
+	        a=10;
+	        b=0;
+	        c=a/b;
+	      System.out.println(c);
+		}catch(Exception ae) {
+			System.out.println("canot divede bt zero");
+	 }
+		finally {
+			System.out.println("end");
+		}
+		
+		
+		
+		
+		
 	}
 
 }
